6a2d1d6a7fb74e49115a068ae8a926973941b0a0
Mayur Kawale <122032765+Mefisto04@users.noreply.github.com>
2023-08-17 20:57:52 +0530

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int a = 123;
+    cout << a << endl;
+    char ch = 'a';
+    cout << ch << endl;
+    float f = 1.2;
+    cout << f << endl;
+    bool bl = true;
+    cout << bl << endl;
+    double d = 1.23;
+    cout << d << endl;
+    int size = sizeof(d);
+    cout << "Size of d is : " << size << endl;
+}
+
+
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     int a = 'a';
+//     cout << a << endl;
+//     char ch = 98;
+//     cout << ch << endl;
+//     char ch1 = 1236;
+//     cout << ch1 << endl;
+// }
+
