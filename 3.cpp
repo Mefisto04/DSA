3fd7bebcddd6cbf9d97452d9542b628ed52ef921
Mayur Kawale <122032765+Mefisto04@users.noreply.github.com>
2023-08-17 20:58:27 +0530

    Add files via upload

---
+++ 3.cpp
@@ -0,0 +1,194 @@
+// #include <iostream>
+// using namespace std;
+// int main() {
+//     int n;
+//     cout<<"Enter the value of n : ";
+//     cin>>n;
+//     if(n<0) {
+//         cout<<"n is negative !"<<endl;
+//     } else if(n>0) {
+//         cout<<"n is positive !"<<endl;
+//     } else {
+//         cout<<"n is zero !"<<endl;
+//     }
+// }
+
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int a, b;
+
+//     cout<<"Enter the value of a : ";
+//     cin>>a;
+
+//     cout<<"Enter the value of b : ";
+//     cin>>b;
+
+//     if(a>b) {
+//         cout<<"a is greater"<<endl;
+//     } else {
+//         cout<<"b is greater"<<endl;
+//     }
+// }
+
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     char ch;
+
+//     cout<<"Enter the character : ";
+//     cin>>ch;
+
+//     if(ch>='A' && ch<='Z') {
+//         cout<<"Uppercase Character !"<<endl;
+//     } else if(ch>='a' && ch<='z') {
+//         cout<<"Lowercase Character !"<<endl;
+//     } else {
+//         cout<<"Number !"<<endl;
+//     }
+// }
+
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int n;
+
+//     cout<<"Enter the value of n : ";
+//     cin>>n;
+
+//     int i = 1;
+
+//     while(i <= n) {
+//         cout<<i<<" ";
+//         i = i+1;
+//     }
+// }
+
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int n;
+
+//     cout<<"Enter the value of n : ";
+//     cin>>n;
+
+//     int i = 1;
+//     int sum = 0;
+
+//     while(i <= n) {
+//         sum = sum + i;
+//         i = i + 1;
+//     }
+
+//     cout<<"Sum of n numbers = "<<sum<<endl;
+// }
+
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     int n;
+
+//     cout<<"Enter the value of n : ";
+//     cin>>n;
+
+//     int i = 2;
+//     int sum = 0;
+
+//     while(i <= n) {
+//         sum = sum + i;
+//         i = i + 2;
+//     }
+
+//     cout<<"Sum of even numbers upto n = "<<sum<<endl;
+// }
+
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     float f;
+
+//     cout<<"Enter the temperature in fahrenheit : ";
+//     cin>>f;
+
+//     float c = ((f-32) * 5) / 9;
+
+//     cout<<"Temperature in celsius : "<<c<<endl;
+// }
+
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     /*
+//         * * * *
+//         * * * *
+//         * * * *
+//         * * * *
+//     */
+
+//     int n;
+
+//     cout<<"Enter the value of n : ";
+//     cin>>n;
+
+//     int i = 1;
+
+//     while(i <= n) {
+//         int j = 1;
+
+//         while(j<=n) {
+//             cout<<"* ";
+//             j = j + 1;
+//         }
+
+//         cout<<endl;
+//         i = i + 1;
+//     }
+// }
+
+
+// #include <iostream>
+// using namespace std;
+
+// int main() {
+//     /*
+//         1 1 1 1
+//         2 2 2 2
+//         3 3 3 3
+//         4 4 4 4
+//     */
+
+//     int n;
+
+//     cout<<"Enter the value of n : ";
+//     cin>>n;
+
+//     int i = 1;
+
+//     while(i <= n) {
+//         int j = 1;
+
+//         while(j<=n) {
+//             cout<<i<<" ";
+//             j = j + 1;
+//         }
+
+//         cout<<endl;
+//         i = i + 1;
+//     }
+// }
+
+
---
+++ 6.cpp
@@ -0,0 +1,96 @@
+// #include <iostream>
+// using namespace std;
+
+// int pow(int n, int p) {
+//     return (p==0) ? 1 : n*pow(n, p-1);
+// }
+
+// int main() {
+//     int n;
+//     cout<<"Enter the value of n : ";
+//     cin>>n;
+
+//     int answer = 0;
+//     int i = 0;
+
+//     while(n != 0) {
+//         int bit = n & 1;
+
+//         answer = answer + (bit * pow(10,i));
+
+//         i++;
+//         n = n >> 1;
+//     }
+
+//     cout<<"Converted Binary Number : "<<answer<<endl;
+
+//     return 0;
+// }
+
+
+
+// #include <iostream>
+// using namespace std;
+
+// int pow(int n, int p) {
+//     return (p==0) ? 1 : n*pow(n, p-1);
+// }
+
+// int main() {
+//     int n;
+//     cout<<"Enter a negative number : ";
+//     cin>>n;
+
+//     n *= (-1);      //Converting the negative number into positive to find its binary representation
+
+//     int answer = 0;
+//     int i = 0;
+
+//     n = (~n);       // Getting the 1's compliment of the number
+//     n = n + 1;      // Adding 1 in order to obtain the 2's compliment of the number
+
+//     while(n != 0 && n != -1) {
+//         int bit = n & 1;
+
+//         answer = answer + (bit * pow(10,i));
+
+//         i++;
+//         n = n >> 1;
+//     }
+
+//     cout<<"Converted Binary Number : "<<answer<<endl;
+
+//     return 0;
+// }
+
+
+// #include <iostream>
+// using namespace std;
+
+// int pow(int n, int p) {
+//     return (p==0) ? 1 : n*pow(n, p-1);
+// }
+
+// int main() {
+//     int n;
+//     cout<<"Enter the value of n : ";
+//     cin>>n;
+
+//     int answer = 0;
+//     int i = 0;
+
+//     while(n != 0) {
+//         int digit = n % 10;
+
+//         if(digit == 1) {
+//             answer = answer + pow(2,i);
+//         }
+
+//         i++;
+//         n /= 10;
+//     }
+
+//     cout<<"Converted Binary Number : "<<answer<<endl;
+
+//     return 0;
+// }
